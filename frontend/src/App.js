365b8f09e7046007f6952ccfab00ab21c056633f
Stuart Reilly <stuartcreilly@isquaredmaths.com>
2023-12-28 23:11:37 +0000

    First api call section 7-40

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header';
 import Search from './components/Search';
 import { Container } from 'react-bootstrap';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 
 const App = () => {
   
@@ -11,7 +13,15 @@ const App = () => {
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word)
+    console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
 }
 
   return (
