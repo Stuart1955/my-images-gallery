d4a93c014ad351b3ad8277cb06dcdfacc7ad8aa1
Stuart Reilly <stuartcreilly@isquaredmaths.com>
2023-12-28 10:54:47 +0000

    Controlled search component section 6.35

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+import { Container } from 'react-bootstrap';
+
 
 const App = () => {
+  
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word)
+}
+
   return (
+    <Container>
     <div className="App">
       <Header title="My Images Gallery"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
+    </Container>
   );
 }
 
