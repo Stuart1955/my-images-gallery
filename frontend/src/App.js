1e44577ef83765b2e1f51191c0d190f33634a9a9
Stuart Reilly <stuartcreilly@isquaredmaths.com>
2023-12-31 23:40:10 +0000

    fixed code with lint and prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,19 @@
-import { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import Search from "./components/Search";
-import { Container } from "react-bootstrap";
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
+import { Container } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
-
 const App = () => {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
     fetch(
-      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
     )
       .then((res) => res.json())
       .then((data) => {
@@ -23,7 +22,7 @@ const App = () => {
       .catch((err) => {
         console.log(err);
       });
-    setWord("");
+    setWord('');
   };
 
   return (
